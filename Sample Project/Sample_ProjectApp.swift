f181302e8cd4eabcb50940826d1045c0f202fa06
tcowan <tcowan@my321.net>
2022-06-01 12:09:29 -0600

    Initial Commit

---
+++ Sample Project/Sample_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_ProjectApp.swift
+//  Sample Project
+//
+//  Created by Ted Cowan on 6/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Sample_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
